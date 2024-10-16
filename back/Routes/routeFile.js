4619bb8ea8a268f38bfc03c0beb2a95a848ad4cb
Njato <m.pdfb002.njato.optimumsolutions@gmail.com>
2024-10-16 11:34:29 +0300

    Home file display

--- back/Routes/routeFile.js
+++ back/Routes/routeFile.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
     storage: storage,
     fileFilter: function (req, file, cb) {
       // Accepter uniquement les fichiers PDF
-      if (file.mimetype === 'application/pdf') {
+      if (file.mimetype === 'application/pdf' || file.mimetype === 'text/xml' ) {
         cb(null, true);
       } else {
         cb(new Error('Only PDF files are allowed'), false);
