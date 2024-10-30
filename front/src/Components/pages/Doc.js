d7c0b9e9c688d7ce8e810438353948c9c2416856
Onisoa Tahiana RAZANAKINIAINA <onisoa.pdfb-001@solumada.local>
2024-10-30 08:26:44 +0300

    delete variable non utilisé

--- front/src/Components/pages/Doc.js
+++ front/src/Components/pages/Doc.js
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import Input from "../others/Input";
 import MyDocument from "../others/MyDocument";
 import { useNavigate, useParams } from "react-router-dom";
-import { changeObjectValue, GenerateXMLFromResponse, SERVER_URL } from '../../utils/utils';
+import { changeObjectValue, GenerateXMLFromResponse } from '../../utils/utils';
 import service from '../services/fileService'
 import ValidationSteps from "../others/ValidationSteps";
 import { Alert, Button, Skeleton, Snackbar } from '@mui/material'
@@ -36,13 +36,11 @@ const Doc = () => {
   const [invoiceData, setInvoiceData] = useState({});
   const [searchText, setSearchText] = useState('');
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
   const [snackAlert, setSnackAlert] = useState(defaultSnackAlert);
   const [dialogComment, setDialogComment] = useState(defaultSnackAlert);
   const [loadingState, setLoadingState] = useState(defaultLoadingState);
   // get active user infos from localstorage
   const _User = JSON.parse(localStorage.getItem('user'));
-  console.log(_User)
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
@@ -54,7 +52,7 @@ const Doc = () => {
 
   useEffect(() => {
 
-    if (!v) navigate('/home');
+    if (!v) navigate('/');
 
 
     // check validation
@@ -202,7 +200,7 @@ const Doc = () => {
       versionNumber: validationStage
     }).then(async res => {
 
-      const { data, ok } = await res;
+      const {  ok } = await res;
 
       if (ok) {
         if (validationStage === 'v2') {
